74fc41f822dfb7446ba491d4021356b02b7ddbe4
adam5096 <adamchou5096@gmail.com>
2025-08-07 15:52:05 +0800

    feat(users/settings): 新增 ui 第一部分: 體驗者帳戶中心

--- pages/users/settings/index.vue
+++ pages/users/settings/index.vue
@@ -1,6 +1,35 @@
+<script setup lang="ts">
+import UserIcon from '~/components/UserIcon.vue';
+
+definePageMeta({
+  name: 'user-settings',
+  layout: 'user',
+});
+</script>
+
 <template>
-  <div>
-    <h1>Settings</h1>
-  </div>
+  <main class="min-h-screen bg-brand-gray">
+    <div class="mx-auto max-w-container-users px-6 py-12 md:px-12">
+      <div class="mb-8">
+        <h1 class="text-2xl font-bold text-primary-blue-dark">
+          帳戶中心設置
+        </h1>
+      </div>
+
+      <div class="flex items-center gap-6">
+        <div class="flex h-20 w-20 items-center justify-center rounded-full bg-gray-200">
+          <UserIcon class="h-12 w-12 text-gray-500" />
+        </div>
+        <div>
+          <p class="text-xl font-bold text-primary-blue-dark">
+            林威辰
+          </p>
+          <p class="text-sm text-gray-500">
+            台灣體驗管理員
+          </p>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
