86bf0e17db3a193e10e9ca9785ce4c09fe06980c
adam5096 <adamchou5096@gmail.com>
2025-08-20 14:46:34 +0800

    feat(company): 確立 API 整合策略、設定環境變數、實踐安全性

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
   ],
   runtimeConfig: {
     public: {
-      apiBase: '/api',
+      apiBase: process.env.NUXT_PUBLIC_API_BASE_URL || '/api',
     },
   },
   fonts: {
