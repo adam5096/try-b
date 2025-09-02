c7e660cc3ab372033e560ee16448b893f1df6e46
adam5096 <adamchou5096@gmail.com>
2025-09-02 13:03:00 +0800

    fix(users): 調整樣式

--- pages/users/comments/index.vue
+++ pages/users/comments/index.vue
@@ -348,10 +348,16 @@ onMounted(() => {
     <!-- Pagination -->
     <div v-if="!loading && !error" class="mt-6 flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
       <!-- 每頁顯示 -->
-      <div class="flex items-center gap-3">
+      <div class="flex items-center gap-3 whitespace-nowrap">
         <span class="text-gray-600">每頁顯示：</span>
-        <el-select v-model="pageSize" class="w-full min-w-form-control md:max-w-form-select">
-          <el-option v-for="size in pageSizeOptions" :key="size" :label="`${size} 筆`" :value="size" />
+        <el-select v-model="pageSize" placeholder="選擇">
+          <el-option 
+            v-for="size in pageSizeOptions" 
+            :key="size" 
+            :value="size"
+          >
+            {{ size }} 筆
+          </el-option>
         </el-select>
       </div>
 
